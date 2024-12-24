77bd8a46cdc5787a96ec660f47a807d59b2a1ce1
Timurbek <timurbeksaburovv.@gmail.com>
2024-12-24 14:02:17 +0200

    day-4-takrorlash-js

---
+++ script.js
@@ -0,0 +1,22 @@
+const backToTopButton = document.getElementById("backToTop");
+window.onscroll = function () {
+    scrollFunction();
+};
+function scrollFunction() {
+    if(
+        document.body.scrollTop > 200 ||
+        document.documentElement.scrollTop > 200
+    ) {backToTopButton.classList.add("show");
+    } else {
+        backToTopButton.classList.remove("show");
+    }
+
+    
+}
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    })
+}
+backToTopButton.addEventListener("click", scrollToTop);
